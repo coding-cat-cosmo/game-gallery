bbfa8ec36adcd45ce4c8eb1a444b4ef9cb8eb85f
coding-cat-cosmo <huntmatt524@gmail.com>
2021-02-17 01:22:33 -0800

    made inital state of project to facilitate starting it

---
+++ header/videogame.hpp
@@ -0,0 +1,49 @@
+#ifndef VIDEOGAME_HPP
+#define VIDEOGAME_HPP
+#include <iostream>
+using namespace std;
+
+class VideoGame {
+	private:
+		string name;
+		unsigned int year;
+		string publisher;
+		string system;
+		string genre;
+		char rating;
+		string size;
+		unsigned int cost;
+		unsigned int players;
+		unsigned int id;
+		static unsigned int newID = 1; 
+	public:
+		VideoGame() : name("Galaga"), year(1999), publisher("ATARI"), system("Arcade Machine"), 
+		genre("shooter"), rating('E'), size("2.5 GB"), cost(35), players(1), { id = newID++; }
+		VideoGame(string nam, int yr, string pub, string sys, string gen, char rat, string mem,
+		 int value, int play) : name(nam), year(yr), publisher(pub), system(sys), genre(gen), 
+		 rating(rat), size(mem), cost(value), players(play) { id = newID++;}
+
+		~VideoGame() {}
+		void print();
+		int getId();
+		string getName();
+		int getYear();
+		string getPub();
+		string getSystem();
+		string getGenre();
+		char getRating();
+		string getSize();
+		int getCost();
+		int getPlayer();
+		void setName(string);
+		void setYear(int);
+		void setPub(string);
+		void setSystem(string);
+		void setGenre(string);
+		void setRating(string);
+		void setSize(string);
+		void setCost(int);
+		void setPlayer(int);
+};
+		
+#endif //VIDEOGAME_HPP
---
+++ src/videogame.cpp
@@ -0,0 +1,85 @@
+#include "../header/videogame.h"
+
+	void VideoGame::print() {
+        cout << "ID: " << getId() << " Name: " << getName() << " Year: " << getYear() 
+        << " Publisher: " << getPub() << " System: " << getSystem() 
+        << " Genre: " << getGenre() << " Rating: " << getRating() << " Size: " << getSize()
+        << " Cost: " << getCost() << " Player: " << getPlayer() << endl << endl;
+    }
+
+	int VideoGame::getId() {
+        return id;
+    }
+
+	string VideoGame::getName() {
+        return name;
+    }
+
+	int VideoGame::getYear() {
+        return year;
+    }
+
+	string VideoGame::getPub() {
+        return publisher;
+    }
+
+	string VideoGame::getSystem() {
+        return system;
+    }
+
+	string VideoGame::getGenre() {
+        return genre;
+    }
+
+	char VideoGame::getRating() {
+        return rating;
+    }
+
+	string VideoGame::getSize() {
+        return size;
+    }
+
+	int VideoGame::getCost() {
+        return cost;
+    }
+
+	int VideoGame::getPlayer() {
+        return player;
+    }
+
+	void VideoGame::setName(string nam) {
+        name = nam;
+    }
+
+	void VideoGame::setYear(int yr) {
+        year = yr
+    }
+
+	void VideoGame::setPub(string pub) {
+        publisher = pub;
+    }
+
+	void VideoGame::setSystem(string sys) {
+        system = sys;
+    }
+
+	void VideoGame::setGenre(string gen) {
+        genre = gen;
+    }
+
+	void VideoGame::setRating(string rat) {
+        rating = rat;
+    }
+
+	void VideoGame::setSize(string mem) {
+        size = mem;
+    }
+
+	void VideoGame::setCost(int money) {
+        cost = money;
+    }
+
+	void VideoGame::setPlayer(int play) {
+        player = play;
+    }
+
