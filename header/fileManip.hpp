9f933dd807f2d66ac3c90deed5e93d656fc0d360
codyphu <codyphu168@yahoo.com>
2021-02-24 22:16:21 -0800

    fixed more issues

--- header/fileManip.hpp
+++ header/fileManip.hpp
@@ -5,20 +5,56 @@
 #include "nlohmann/json.hpp"
 #include <iostream>
 
-
 // for convenience
 using json = nlohmann::json;
 
+/**
+ 		* @brief: This class has all the main functionality for any desirable action to be ran on the videoGame database
+ 		* 
+		*/
+
 class fileManip{
 	private:
-		std::string filename;
+	std::string filename;
 
 	public:
-		fileManip(std::string name);
-		~fileManip(){}
-		void save(json j);
-		json load();
-		json newJSON();
+	/**
+		* @brief: constructor of the fileManip class that initializes with passed in objects
+		* 
+		* @param: None
+		* @return: fileManip Object
+		*/
+	fileManip(std::string name);
+	/**
+  		* @brief: destructor of the fileManip class
+  	 	* 
+  	 	* @param: None
+  	 	* @return: fileManip Object
+  	 	*/
+	~fileManip(){}
+	/**
+ 	 	 * @brief: saves the file to the database
+  	 	 * 
+  	 	 * @param: json j for json object to save to file
+  	 	 * @return: None
+  	 	 */
+	void save(json j);
+        /**
+         	* @brief: load the file from the database
+         	*            
+                * @param: None
+                * @return: retunrs a JSON object
+                */
+
+	json load();
+        /**           
+  		 * @brief: creates a new JSON file into the database
+  		 *
+  		 * @param: None
+  		 * @return: returns a JSON object
+                 */
+
+	json newJSON();
 };
 
 
