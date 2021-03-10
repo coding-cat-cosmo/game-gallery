1543af74cf186076ea99999d31266df5954e2cc6
Cody Phu <codyphu168@yahoo.com>
2021-03-10 01:09:38 -0800

    Update search_genre.hpp

--- header/search_genre.hpp
+++ header/search_genre.hpp
@@ -4,11 +4,32 @@
 #include "collection.hpp"
 #include "search.hpp"
 #include <iostream>
-
+/**
+ 	* @brief: This class is one of the concrete strategies for searching
+ 	* 
+	*/
 class SearchGenre: public Searcher{
         public:
+	/**
+  	 * @brief: constructor of the SearchGenre class
+  	 * 
+  	 * @param: None
+  	 * @return: SearchGenre object
+  	 */
 	SearchGenre(){}
+	/**
+  	 * @brief: destructor of the SearchGenre class
+  	 * 
+  	 * @param: None
+  	 * @return: SearchGenre object
+  	 */
 	~SearchGenre(){}
+        /**
+  	 * @brief: virtual searcher function
+  	 * 
+  	 * @param: gal: pointer to Collection object, s: string object
+  	 * @return: pointer to a VideoGame object
+  	 */
         VideoGame* search(Collection* gal,std::string searchTerm){
                 for(int i=0;i<gal->getCollection().size();++i){
                         if(gal->getCollection()[i]->getGenre()==searchTerm){
