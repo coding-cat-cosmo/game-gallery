42c11d448e071452fb830f2cddac3423c7b54f1f
Cody Phu <codyphu168@yahoo.com>
2021-03-10 01:10:32 -0800

    Update search_name.hpp

--- header/search_name.hpp
+++ header/search_name.hpp
@@ -4,10 +4,25 @@
 #include "collection.hpp"
 #include "search.hpp"
 #include <iostream>
-
+/**
+ 	* @brief: This class is one of the concrete strategies for searching
+ 	* 
+	*/
 class SearchName: public Searcher{
         public:
+	/**
+  	 * @brief: destructor of the SearchName class
+  	 * 
+  	 * @param: None
+  	 * @return: SearchName object
+  	 */
 	~SearchName(){}
+	/**
+  	 * @brief: virtual searcher function
+  	 * 
+  	 * @param: gal: pointer to Collection object, s: string object
+  	 * @return: pointer to a VideoGame object
+  	 */
         VideoGame* search(Collection* gal, std::string searchTerm){
 		
 		for(int i=0;i<gal->getCollection().size();++i){
