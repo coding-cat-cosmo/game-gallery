bd59f903049fccfdeb22370186753e4a194ad4df
Cody Phu <codyphu168@yahoo.com>
2021-03-10 01:11:29 -0800

    Update search_pub.hpp

--- header/search_pub.hpp
+++ header/search_pub.hpp
@@ -4,11 +4,32 @@
 #include "collection.hpp"
 #include "search.hpp"
 #include <iostream>
-
+/**
+ 	* @brief: This class is one of the concrete strategies for searching
+ 	* 
+	*/
 class SearchPub: public Searcher{
         public:
+	/**
+  	 * @brief: constructor of the SearchPub class
+  	 * 
+  	 * @param: None
+  	 * @return: SearchPub object
+  	 */
 	SearchPub(){}
+	/**
+  	 * @brief: destructor of the SearchGenre class
+  	 * 
+  	 * @param: None
+  	 * @return: SearchGenre object
+  	 */
 	~SearchPub(){}
+	/**
+  	 * @brief: virtual searcher function
+  	 * 
+  	 * @param: gal: pointer to Collection object, s: string object
+  	 * @return: pointer to a VideoGame object
+  	 */
         VideoGame* search(Collection* gal, std::string searchTerm){
 
                 for(int i=0;i<gal->getCollection().size();++i){
